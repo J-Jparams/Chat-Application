e8eb379e8ec5c512e6285f004cfa79f5473c6fcc
Justin Hued <huedjustin@gmail.com>
2022-08-21 02:13:41 -0400

    Developed mapping specifications for User Search

--- client/src/Chat/UserSearch.tsx
+++ client/src/Chat/UserSearch.tsx
@@ -1,50 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Css/ChatApp/UserSearch.css';
 import Applogo from '../Assets/Applogo.png'
 import User from './User';
 
 const UserSearch = () => {
+  const [data, setData]:any = useState("");
+
+
+
+
+  useEffect(() => {
+    let unmounted = false;
+    fetch("http://localhost:3002/allUsers")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (!unmounted) {
+          setData(data);
+        }
+      })
+      .catch((err) => {
+        console.log(err.message, "UserSearchError");
+      });
+
+    return () => {
+      unmounted = true;
+    };
+  }, []);
 
   return (
-    <div id='UserSearch'>
-      <div className='searchUser'>
-        <input type='text' placeholder='Search User'/>
+    <>
+      <div id='UserSearch'>
+        <div className='searchUser'>
+          <input type='text' placeholder='Search User'/>
+        </div>
+
+        <div className='users'>
+        <div>
+          {data &&
+            data?.map((post:any, index:any) => {
+              return (
+                <User 
+                  key={index}
+                  userIcon={post.userIcon}
+                  userName={post.userName}
+                />
+              )
+            })
+          }
+        </div>
+          {/* <User userIcon='' userName=''/> */}
+        </div>
       </div>
-
-      <div className='users'>
-       
-       <User userIcon='' userName=''/>
-
-        <div className='user'>
-          <img className='userIcon' src={Applogo} alt='PFP' height='40px'/>
-          <div className='userRight'>
-            <h3 className='userName'>
-              User Name
-            </h3>
-          </div>
-        </div>
-
-        <div className='user'>
-          <img className='userIcon' src={Applogo} alt='PFP' height='40px'/>
-          <div className='userRight'>
-            <h3 className='userName'>
-              User Name
-            </h3>
-          </div>
-        </div>
-
-        <div className='user'>
-          <img className='userIcon' src={Applogo} alt='PFP' height='40px'/>
-          <div className='userRight'>
-            <h3 className='userName'>
-              User Name
-            </h3>
-          </div>
-        </div>
-
-      </div>
-
-    </div>
+    </>
   )
 }
 

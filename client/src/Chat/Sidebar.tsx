5030f05a3f6e7d4d73a5ac625b959355c94bb4ab
Justin Hued <huedjustin@gmail.com>
2022-08-20 22:17:55 -0400

    Completed Sidebar Design

--- client/src/Chat/Sidebar.tsx
+++ client/src/Chat/Sidebar.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Css/ChatApp/Sidebar.css';
 import Applogo from '../Assets/Applogo.png';
+import GroupChat from './GroupChat';
 
 const Sidebar = () => {
+  // const [data, setData] = useState;
+
   return (
     <div id='Sidebar'>
       <div className='searchChat'>
         <input placeholder='Search'/>
       </div>
       <div className='chats'>
-        <div className='chat'>
-  
-          <div className='chatIcon'>
-            <img src={Applogo} height='40px'/>
-          </div>
+        {/* The Line below is for mapping groupChats */}
+        {/* <div>
+          {data &&
+            data.map((post:any, index:any) => {
+              return (
+                <GroupChat 
+                  key={index}
+                  groupName={post.groupName} 
+                  lastChat={post.lastChat} 
+                  lastChatDate={post.lastChatDate}
+                  chatIcon={Applogo}
+                />
+              )
+            })
+          }
+        </div> */}
+        <GroupChat 
+          groupName="Satchel Boys" 
+          lastChat={"What"} 
+          lastChatDate='2/1/22' 
+        />
 
-          <div className='chatRight'>
-            <h3>Group Name</h3>
-            <p>Last Chat</p>
-          </div>
-
-          <div className='chatDate'>
-            Sunday
-          </div>
-        </div>
       </div>
     </div>
   )

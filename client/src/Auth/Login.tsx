a6ac2925941e4aa97e9bc8c7618291f2a1a148f3
Justin Hued <huedjustin@gmail.com>
2022-09-10 23:29:14 -0400

    Changed Design

--- client/src/Auth/Login.tsx
+++ client/src/Auth/Login.tsx
@@ -1,48 +1,53 @@
 import React from 'react';
 import '../Css/Login.css';
-import Applogo from '../Assets/Applogo.png';
+import Applogo2 from '../Assets/Applogo2.png';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div className='Login'>
-      <div className='login-box'>
-        <div className='login-title'>
-          <img src={Applogo} alt="logo" />
-          <h1 className='text'>QUADNARD CHAT</h1>
-        </div>
-        <form>
-          
-          <input 
-            type='text'
-            className='login-inputs'
-            placeholder='Username...'
-            required 
-          />
-          
-          <input 
-            type='password'
-            className='login-inputs'
-            placeholder='Password...' 
-            required 
-          />
-                   
-          <button
-            type='submit'
-            className='login-btn'
-          >
-          Register
-          </button>
-
-          <Link to='/'>
-            <button className='login-signup'>
-              Have An Account?
-            </button>
-          </Link>
-
-        </form>
+    <>
+      <div className='left-Login'>
+        <img src={Applogo2} alt="logo" height="300px"/>
       </div>
-    </div>
+      <div className='Login'>
+        <div className='login-box'>
+          <div className='login-title'>
+            <h1 className='before'>What Are You <br /> Waiting For?</h1>
+            <h1 className='text'>All That's Left <br /> Is To Login!</h1>
+          </div>
+          <form>
+            
+            <input 
+              type='text'
+              className='login-inputs'
+              placeholder='Username...'
+              required 
+            />
+            
+            <input 
+              type='password'
+              className='login-inputs'
+              placeholder='Password...' 
+              required 
+            />
+                    
+            <button
+              type='submit'
+              className='login-btn'
+            >
+            Login
+            </button>
+
+            <Link to='/'>
+              <button className='login-signup'>
+                Need An Account?
+              </button>
+            </Link>
+
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
--- client/src/Auth/SignUp.tsx
+++ client/src/Auth/SignUp.tsx
@@ -1,55 +1,60 @@
 import React from 'react';
 import '../Css/SignUp.css';
-import Applogo from '../Assets/Applogo.png';
+import Applogo2 from '../Assets/Applogo2.png';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   return (
-    <div className='SignUp'>
-      <div className='signup-box'>
-        <div className='signup-title'>
-          <img src={Applogo} alt="logo" />
-          <h1 className='text'>QUADNARD CHAT</h1>
-        </div>
-        <form>
-          
-          <input 
-            type='text'
-            className='signup-inputs'
-            placeholder='Username...'
-            required 
-          />
-          
-          <input 
-            type='password'
-            className='signup-inputs'
-            placeholder='Password...' 
-            required 
-          />
-                   
-          <input 
-            type='password'
-            className='signup-inputs' 
-            placeholder='Confirm Password...' 
-            required 
-          />
-
-          <button
-            type='submit'
-            className='signup-btn'
-          >
-          Register
-          </button>
-
-          <Link to='/login'>
-            <button className='signup-login'>
-              Have An Account?
-            </button>
-          </Link>
-
-        </form>
+    <>
+      <div className='left-Register'>
+        <img src={Applogo2} alt="logo" height="300px"/>
       </div>
-    </div>
+      <div className='SignUp'>
+        <div className='signup-box'>
+          <div className='signup-title'>
+            <h1 className='before'>What Are You <br /> Waiting For?</h1>
+            <h1 className='text'>Join Lingo Today.</h1>
+          </div>
+          <form>
+            
+            <input 
+              type='text'
+              className='signup-inputs'
+              placeholder='Username...'
+              required 
+            />
+            
+            <input 
+              type='password'
+              className='signup-inputs'
+              placeholder='Password...' 
+              required 
+            />
+                    
+            <input 
+              type='password'
+              className='signup-inputs' 
+              placeholder='Confirm Password...' 
+              required 
+            />
+
+            <button
+              type='submit'
+              className='signup-btn'
+            >
+            Register
+            </button>
+
+            <Link to='/login'>
+              <button className='signup-login'>
+                Have An Account?
+              </button>
+            </Link>
+
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
